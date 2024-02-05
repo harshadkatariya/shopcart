2ad52779fa968c898190b7131eadf64a3b995341
harshadkatariya <48067159+harshadkatariya@users.noreply.github.com>
2024-02-05 15:02:45 +0530

    Add files via upload

---
+++ src/Components/LoginForm/LoginForm.jsx
@@ -0,0 +1,84 @@
+ // Import necessary dependencies
+import React, { useState } from 'react';
+import './LoginForm.css';
+ import cart from './cart1.svg';
+import {NavLink} from 'react-router-dom';
+
+// Create a functional component for the Register/Login page
+const AuthPage = () => {
+  // State for managing form input values
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isRegistering, setIsRegistering] = useState(true); // State to switch between Register and Login forms
+
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Perform registration or login logic based on isRegistering state
+    if (isRegistering) {
+      // Registration logic
+      console.log('Registering with email:', email, 'and password:', password);
+    } else {
+      // Login logic
+      console.log('Logging in with email:', email, 'and password:', password);
+    }
+
+    // Clear form fields after submission
+    setEmail('');
+    setPassword('');
+  };
+
+  const Headr = () =>{
+    return (
+      <div className='hdr'> 
+        <h1>ShopKart</h1>
+        <div className='nav'>
+          <a href='#product'>Product</a>
+          <a href='#Login'>Login</a>
+          <img src={require('./cart.png')} />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className='lg-div'>
+               
+        <Headr />
+    
+        <div className='main_form_div'>
+            
+        <h2>{isRegistering ? 'Login' : 'Register'}</h2>
+         
+        <form onSubmit={handleSubmit}>
+            {/* Email input */}
+            <input type="email" value={email} placeholder='Email Address' onChange={(e) => setEmail(e.target.value)} required />
+            <br></br>
+            <br></br>
+
+            {/* Password input */}
+            <input type="password" value={password} placeholder='Password' onChange={(e) => setPassword(e.target.value)} required />
+            
+
+            {/* Submit button */}
+            
+        </form>
+         
+        
+        {/* Switch between Register and Login */}
+        <div className='btn-main'>
+            <p onClick={() => setIsRegistering(!isRegistering)}>
+            {isRegistering ? 'New User? Create Account.':'Already have an account? Login here.'}
+            </p>
+            <button type="submit">{isRegistering ? 'Login' : 'Register'}</button>
+        
+        </div>
+        
+        </div>
+    </div>
+  );
+};
+
+// Export the component
+export default AuthPage;
